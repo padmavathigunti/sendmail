45c735a277c659c5cc64c0dd28a4491133ed1cac
guntipadmavati <padmavathigunti422@gmail.com>
2019-10-28 22:30:39 +0530

    sendmail

---
+++ src/sendmail/sendmail/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+
+
+def home(request):
+    return render(request,'home.html')
+
+
+def sendmail(request):
+    subject = 'Thank you for registering to our site'
+    message = ' it  means a world to us '
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = ['guntipadmavathi1@gmailcom',]
+    send_mail( subject, message, email_from, recipient_list )
+
+
+    return HttpResponse('Mail successfully sent')
